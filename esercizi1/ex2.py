10b17f0e4ba9f759d9a7110fa4a9a57dd023770f
PikusFikus1 <93773534+PikusFikus1@users.noreply.github.com>
2022-11-17 13:56:49 +0000

    SHAMPOOOOOOOO

--- esercizi1/ex2.py
+++ esercizi1/ex2.py
@@ -1,8 +1,8 @@
-def sum_csv():
+def sum_csv(file_name):
     values = []
     tot = 0
 
-    my_file = open('shampoo_sales.csv', 'r')
+    my_file = open(file_name, 'r')
     for line in my_file:
 
         elements = line.split(',')
